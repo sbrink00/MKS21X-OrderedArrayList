c04d9b7acc7bf7c3877fbc22760bc6328c1fca8b
sbrink00 <sbrink00@stuy.edu>
2018-11-01 11:48:26 -0400

    wrote both adds and set.

---
+++ NoNullArrayList.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+public class NoNullArrayList<T> extends ArrayList<T>{
+
+  public NoNullArrayList(){}
+
+  public NoNullArrayList(int startSize){
+    super(startSize);
+  }
+
+  public boolean add(T o){
+    if (o == null){
+      throw new IllegalArgumentException("null can't be added");
+    }
+    else super.add(o);
+    return true;
+  }
+
+  public void add(int index, T o){
+    if (o == null){
+      throw new IllegalArgumentException("null can't be added");
+    }
+    else super.add(index, o);
+  }
+
+  public T set(int index, T o){
+    T output = this.get(index);
+    if (o == null){
+      throw new IllegalArgumentException("value can't be set to null");
+    }
+    else super.set(index, o);
+    return output;
+  }
+
+}
