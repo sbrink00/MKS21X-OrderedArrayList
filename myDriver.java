1095db0a8d30320b700cbb5f7ff710dee0c66471
sbrink00 <sbrink00@stuy.edu>
2018-11-04 22:09:15 -0500

    this is the driver I wrote to test my code.

--- myDriver.java
+++ myDriver.java
@@ -1,6 +1,7 @@
 public class myDriver{
   public static void main(String[]args){
     NoNullArrayList<String> a = new NoNullArrayList<>();
+    /*
     a.add("A");
     a.add("P");
     a.add("U");
@@ -14,8 +15,37 @@ public class myDriver{
     }
     System.out.println(a);
 
+    try{
+      a.add(1, null);
+    }
+    catch (IllegalArgumentException e){
+      e.printStackTrace();
+    }
 
+    try{
+      a.set(2, null);
+    }
+    catch (IllegalArgumentException e){
+      e.printStackTrace();
+    }*/
 
+    OrderedArrayList<String> b = new OrderedArrayList<>();
+    b.add("a");
+    b.add("A");
+    b.add("D");
+    b.add("E");
+    b.add("B");
+    b.add("Cat");
+    b.add("a");
+
+    System.out.println(b);
+
+    OrderedArrayList<Double> myGrades = new OrderedArrayList<>();
+    myGrades.add(95.6);
+    myGrades.add(90.6);
+    myGrades.add(0, 100.5345);
+    myGrades.set(1, 105.90);
+    System.out.println(myGrades);
 
 
 
